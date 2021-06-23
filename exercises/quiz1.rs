ac951280b5cf5f5658c4782e04305d6a17130a25
raen <raen@quantumtrip.org>
2021-06-22 20:33:46 -0400

    Commit changes

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,17 +7,24 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn ..... {
+
+fn calculate_apple_price(count: i32) -> i32 {
+    if count > 40 {
+        count * 1
+    } else {
+        count * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
 fn verify_test() {
     let price1 = calculate_apple_price(35);
     let price2 = calculate_apple_price(65);
-
+    let price3 = calculate_apple_price(5);
     assert_eq!(70, price1);
     assert_eq!(65, price2);
+    assert_eq!(10, price3);
 }

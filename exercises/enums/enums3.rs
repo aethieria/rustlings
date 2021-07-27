02fe015e1f8195b7ab3e1181bfcd1acd40a40211
raen <raen@quantumtrip.org>
2021-07-27 15:08:12 -0400

    Finished quiz 2

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,11 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -13,7 +14,7 @@ struct Point {
 }
 
 struct State {
-    color: (u8, u8, u8),
+    color: ((u8, u8, u8)),
     position: Point,
     quit: bool,
 }
@@ -37,6 +38,13 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+
+        match message {
+            Message::ChangeColor(c) => self.change_color(c),
+            Message::Echo(s) => self.echo(s),
+            Message::Move(p) => self.move_position(p),
+            Message::Quit => self.quit()
+        }
     }
 }
 
